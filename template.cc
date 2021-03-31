bf1f95cc2c24f8f9f636e4672d40b6aa5a452520
jellyfish26 <jellyfish0162@gmail.com>
2021-03-31 23:04:07 +0900

    Add template

---
+++ template.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define PI acos(-1)
+#define vi vector<int>
+const int kMod = 1e9 + 7;
+const int kInf = (~(1<<31)) / 2 - 500;
+const ll kInfL = (~(1LL<<63)) / 2 - 2000;
+
+int main() {
+}
